844ee9b9582c46d12edaca89a4d1a7b259998451
Boris Gorgona <boris.gorgona@gmail.com>
2022-01-13 10:02:31 -0500

    Inicio Practicas JavaScript

---
+++ script.js
@@ -0,0 +1,4 @@
+var Name = prompt("Ingrese el nombre: ");
+var Age = prompt("Ingrese la edad: ");
+var Phone = prompt("Ingrese el Phone: ");
+alert("Nombre: " + Name + "  Edad: " + Age + " Cel: " + Phone);
